41dfdea264e3388c7452cd162b52246b7dcfd946
Quang Anh <you@example.com>
2022-07-12 21:05:06 +0700

    SS2

---
+++ module 2/src/ss8_CleanCodeAndRefactoring/exercise/Refactoring/controller/Main.java
@@ -0,0 +1,20 @@
+package ss8_CleanCodeAndRefactoring.exercise.Refactoring.controller;
+
+import ss8_CleanCodeAndRefactoring.exercise.Refactoring.Model.TennisGame;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enter first player score");
+        int firstPoint = scanner.nextInt();
+
+        System.out.println("enter second player score");
+        int secondPoint= scanner.nextInt();
+
+        String showResults = TennisGame.getScore(firstPoint,secondPoint);
+        System.out.println(showResults);
+    }
+}
